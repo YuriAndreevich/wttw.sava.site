e71789ce835a01b7655cd6dda5ccd01f1f5af907
Yuri Silin <try2scumme@gmail.com>
2024-12-13 22:20:59 +0300

    1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
 
   resolve: {
     alias: {
-      'components': path.resolve('src/components'),
-      'pages': path.resolve('src/pages'),
-      'assets': path.resolve('src/assets'),
-      'UI': path.resolve('src/components/UI'),
-            },
+      components: path.resolve("src/components"),
+      pages: path.resolve("src/pages"),
+      assets: path.resolve("src/assets"),
+      UI: path.resolve("src/components/UI"),
+    },
   },
 });
-
