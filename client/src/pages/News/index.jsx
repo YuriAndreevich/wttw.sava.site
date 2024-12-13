0ccec318b3bafdc4c6af2b77b61b37e754f49f3d
Yuri Silin <try2scumme@gmail.com>
2024-12-13 21:12:48 +0300

    1

--- client/src/pages/News/index.jsx
+++ client/src/pages/News/index.jsx
@@ -1,10 +1,10 @@
-import  { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { PopularPosts } from "./components/PopularPosts";
 import { getAllPosts } from "../../redux/features/post/postSlice";
-import styles from './News.module.scss';
-import Card from './components/Card'
-import { useLocation, Link } from 'react-router-dom';
+import styles from "./News.module.scss";
+import Card from "./components/Card";
+import { useLocation, Link } from "react-router-dom";
 import { Title } from "../../components/UI";
 
 const News = () => {
@@ -17,32 +17,31 @@ const News = () => {
   }, [dispatch]);
 
   if (!posts || !posts.length) {
-    return (
-      <div className={styles.noPosts}>
-        Новостей не найдены.
-      </div>
-    );
+    return <div className={styles.noPosts}>Новостей не найдены.</div>;
   }
   const array = isMain ? posts.slice(0, 3) : posts;
 
   return (
-
-      <div className={styles.contentWrapper}>
-        {!isMain && (
-         <div className={styles.popularSection}>
+    <div className={styles.contentWrapper}>
+      {!isMain && (
+        <div className={styles.popularSection}>
           <div className={styles.popularTitle}>Популярные новости:</div>
           {popularPosts?.map((post, idx) => (
             <PopularPosts key={idx} post={post} />
           ))}
-        </div>)}
-        <div className={styles.container}>
-          {array?.map((post, idx) => (
-            <Card key={idx} post={post} />
-          ))}
         </div>
-          {isMain && (<Link to='/news'><Title>Читать еще</Title></Link>)}
-       
+      )}
+      <div className={styles.container}>
+        {array?.map((post, idx) => (
+          <Card key={idx} post={post} />
+        ))}
       </div>
+      {isMain && (
+        <Link to="/news">
+          <Title>Читать еще</Title>
+        </Link>
+      )}
+    </div>
   );
 };
 
